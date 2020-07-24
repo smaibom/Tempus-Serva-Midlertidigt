b351c4fec01c9c9fa6b4abeea28f5fe00a5b80aa
smaibom <smaibom@gmail.com>
2020-07-24 10:51:00 +0200

    All used java files

--- Activities.java
+++ Activities.java
@@ -6,12 +6,15 @@
 package activities;
 
 import dk.p2e.blanket.codeunit.CodeunitFormevents;
+import dk.p2e.blanket.form.handler.QueryPart;
 import dk.p2e.util.Parser;
 import dk.p2e.util.Systemout;
 import dk.tempusserva.api.Session;
 import dk.tempusserva.api.SessionFactory;
 import dk.tempusserva.api.SolutionQuery;
+import dk.tempusserva.api.SolutionQueryResultSet;
 import dk.tempusserva.api.SolutionRecord;
+import dk.tempusserva.api.SolutionRecordNew;
 import jdk.nashorn.internal.runtime.regexp.joni.exception.ValueException;
 
 /**
@@ -32,52 +35,207 @@ public class Activities extends CodeunitFormevents{
     
     private final String warehouseEntity = "warehouse";
     
+    private final String activityEntity = "activities";
+    private final String activityCase = "WORKORDER";
+    private final String activityStoppoint = "STOPPOINT";
+    private final String activityDevice = "DEVICE";
+    private final String activityActivity = "ACTIVITY";
+    private final String activityComponentInstalled = "COMPONETTYPEINSTALLED";
+    private final String activityComponentAmountInstalled = "COMPONETTYPEINSTALLEDAMOUNT";
+
+    
+    private final String caseEntity = "workorder";
+    
+    private final String stoppointInvEntity = "stoppointinventory";
+    private final String stoppointInvStoppoint = "STOPPOINT";
+    private final String stoppointInvComponent = "COMPONENT";
+    private final String stoppointInvAmount = "AMOUNT";
+    
+    private final String componentEntity = "components";
+    
+    
+    
+
     @Override
-    public void beforeUpdateItem(){
-        Session ses = SessionFactory.getSession(this);
-        String typeField = c.fields.getElementByFieldName("ACTIVITY").FieldValue;
+    public void beforeUpdateItem() throws Exception{
+        String typeField = c.fields.getElementByFieldName(activityActivity).FieldValue;
         int deviceInstalledDataID = Parser.getInteger(c.fields.getElementByFieldName("DEVICEINSTALLED").FieldValue);
         int deviceUninstalledDataID = Parser.getInteger(c.fields.getElementByFieldName("DEVICEUNINSTALLED").FieldValue);
 
-        int stoppointDataID = Parser.getInteger(c.fields.getElementByFieldName("STOPPOINT").FieldValue);
+        int stoppointDataID = Parser.getInteger(c.fields.getElementByFieldName(activityStoppoint).FieldValue);
         int warehouseDataID = Parser.getInteger(c.fields.getElementByFieldName("TOINVENTORY").FieldValue);
+        int caseDataID = Parser.getInteger(c.fields.getElementByFieldName(activityCase).FieldValue);
+        int componentDataID = Parser.getInteger(c.fields.getElementByFieldName(activityComponentInstalled).FieldValue);
+        int componentAmount = Parser.getInteger(c.fields.getElementByFieldName(activityComponentAmountInstalled).FieldValue);
+        
+        int spcDataID;
+
+        Session ses = SessionFactory.getSession(this);
+
         
         switch(typeField){
             //Change device with new one
             case "170":
-                //if(removeDevice(deviceUninstalledDataID, warehouseDataID)){
-                //    setDeviceStoppoint(deviceInstalledDataID, stoppointDataID, ses);
-                //}
+                try{
+                    SolutionRecord srInstalled = setDeviceStoppoint(deviceInstalledDataID,stoppointDataID,ses);
+                    SolutionRecord srUninstalled = setDeviceStorage(deviceUninstalledDataID, warehouseDataID, ses);
+                    SolutionRecordNew tempName = createSetupActivity(ses, caseDataID, stoppointDataID, deviceInstalledDataID);
+                    c.fields.getElementByFieldName(activityDevice).setFieldValue(deviceUninstalledDataID);
+                    c.fields.getElementByFieldName("DEVICEINSTALLED").setFieldValue("");
+                    c.fields.getElementByFieldName("DEVICEUNINSTALLED").setFieldValue("");
+                    c.fields.getElementByFieldName(activityActivity).setFieldValue(172);
+                    srInstalled.persistChanges();
+                    srUninstalled.persistChanges();
+                    tempName.persistChanges(false);
+                }
+                catch(ValueException e){
+                    c.fields.getElementByFieldName("DEFEKTOKSKALKONTROLLERES").setFieldValue("Dette");
+                    setItemStatus(98);
+                    //Invalid fields
+                }
+                catch(Exception e){
+                    //Figure out a better exception
+                }
                 break;
+                
+                
             //Setup device
             case "171":
                 try{
                     SolutionRecord sr = setDeviceStoppoint(deviceInstalledDataID,stoppointDataID,ses);
+                    c.fields.getElementByFieldName(activityDevice).setFieldValue(deviceInstalledDataID);
+                    c.fields.getElementByFieldName("DEVICEINSTALLED").setFieldValue("");
                     sr.persistChanges();
                 }
                 catch(ValueException e){
                     setItemStatus(98);
+
                     //Invalid fields
                 }
                 catch(Exception e){
                     //Figure out a better exception
                 }
                 break;
+                
+                
             //Remove device from stoppoint
             case "172":
                 try{
-                    SolutionRecord sr = setDeviceStorage(deviceInstalledDataID,warehouseDataID,ses);
+                    SolutionRecord sr = setDeviceStorage(deviceUninstalledDataID,warehouseDataID,ses);
+                    c.fields.getElementByFieldName(activityDevice).setFieldValue(deviceUninstalledDataID);
+                    c.fields.getElementByFieldName("DEVICEUNINSTALLED").setFieldValue("");
+                    
                     sr.persistChanges();
                 }
                 catch(ValueException e){
                     setItemStatus(98);
+
                     //Invalid fields
                 }
                 catch(Exception e){
                     //Figure out a better exception
                 }
                 break;
+                
+                
+            //Restart device, do nothing
+            case "174":
+                try{
+                    if(caseDataID == 0 || stoppointDataID == 0 || deviceUninstalledDataID == 0){
+                        setItemStatus(98);
+                        break;
+                    }
+                    c.fields.getElementByFieldName(activityDevice).setFieldValue(deviceUninstalledDataID);
+                    c.fields.getElementByFieldName("DEVICEUNINSTALLED").setFieldValue("");
+        
+                }
+                catch(Exception e){
+                    
+                }
+                break;
+                
+                
+            //Cablecheck, do nothing
+            case "175":
+                try{
+                    if(caseDataID == 0 || stoppointDataID == 0 || deviceUninstalledDataID == 0){
+                        setItemStatus(98);
+                        break;
+                    }
+                    c.fields.getElementByFieldName(activityDevice).setFieldValue(deviceUninstalledDataID);
+                    c.fields.getElementByFieldName("DEVICEUNINSTALLED").setFieldValue("");
+        
+                }
+                catch(Exception e){
+                    
+                }
+                break;
+                
+                
+                //Component setup
+            case "176":
+                if(componentDataID == 0 || stoppointDataID == 0 || componentAmount < 1){
+                    setItemStatus(98);
+                    break;
+                }
+                spcDataID = findStoppointComponentDataID(componentDataID,stoppointDataID,ses);
+                if(spcDataID == 0){
+                   try{
+                        SolutionRecordNew srn = createStoppointInvComponentRecord(stoppointDataID, componentDataID, componentAmount, ses);
+                        srn.persistChanges();
+                   }
+                   catch(ValueException e){
+                                               setItemStatus(98);
 
+                   }
+                   catch(Exception e){
+                   }
+                }
+                else{
+                    try{
+                        SolutionRecord sr = addStoppointInvComponent(spcDataID, componentAmount, ses);
+                        sr.persistChanges();
+                    }
+                    catch(ValueException e){
+                        //Shouldnt realyl happen as theres a check before
+                        setItemStatus(98);
+                    }
+                    catch(Exception e){
+                    }
+                }
+                break;
+                
+                
+                //Component takedown
+            case "177":
+                if(componentDataID == 0 || stoppointDataID == 0 || componentAmount < 1){
+                    setItemStatus(98);
+                    break;
+                }
+                spcDataID = findStoppointComponentDataID(componentDataID,stoppointDataID,ses);
+                if(spcDataID == 0){
+                    //Do nothing, we cant remove stuff that dont exist
+                }
+                else{
+                    try{
+                        SolutionRecord sr = removeStoppointInvComponent(spcDataID, componentAmount, ses);
+                        sr.persistChanges();
+                    }
+                    catch(ValueException e){
+                        //Shouldnt realyl happen as theres a check before
+                        setItemStatus(98);
+                    }
+                    catch(Exception e){
+                    }
+                }
+                
+                
+                
+                
+                
+                
+                
+                break;
             default:
                 break;
         } 
@@ -86,6 +244,141 @@ public class Activities extends CodeunitFormevents{
         
         
     }
+    
+    
+    
+    
+    
+    //This should be a general function in the future, for now its there
+    private SolutionRecordNew createSetupActivity(Session ses,int caseDataID, int stoppointDataID,int deviceDataID) throws Exception{
+        int stoppointSolutionID = ses.getSolutionID(stoppointEntity);
+        int deviceSolutionID = ses.getSolutionID(deviceEntity);
+        int caseSolutionID = ses.getSolutionID(caseEntity);
+
+        SolutionRecord stoppointSR = ses.getSolutionRecord(stoppointSolutionID, stoppointDataID);
+        SolutionRecord deviceSR = ses.getSolutionRecord(deviceSolutionID, deviceDataID);
+        SolutionRecord caseSR = ses.getSolutionRecord(caseSolutionID, caseDataID);
+
+        if(stoppointSR.getInstanceID() != 0 && deviceSR.getInstanceID() != 0 && caseSR.getInstanceID() != 0){
+            SolutionRecordNew srn = ses.getSolutionRecordNew(activityEntity);
+            srn.setReference(activityCase, caseSR);
+            srn.setReference(activityDevice, deviceSR);
+            srn.setReference(activityStoppoint, stoppointSR);
+            srn.setValueInteger(activityActivity, 171);
+
+            return srn;
+        }
+        else{
+            throw new ValueException("Invalid DataIDs");
+        }
+        
+    }
+    
+    
+    
+    /**
+     * Finds the DataID to the record of a given component type setup at a specific stoppoint
+     * @param componentDataID int value with DataID to the record of the component
+     * @param stoppointDataID int value with DataID to the record of the stoppoint
+     * @param ses Open Session object
+     * @return int value higher than 0 if a record is found, 0 otherwise
+     */
+    private int findStoppointComponentDataID(int componentDataID,int stoppointDataID, Session ses){
+        SolutionQuery q = ses.getSolutionQuery(stoppointInvEntity);
+        q.addWhereCriterion(stoppointInvStoppoint, QueryPart.EQUALS, String.valueOf(stoppointDataID));
+        q.addWhereCriterion(stoppointInvComponent, QueryPart.EQUALS, String.valueOf(componentDataID));
+        SolutionQueryResultSet rs = q.executeQuery();
+        if(rs.size() == 1){
+            return rs.getRecord(0).getInstanceID();
+        }
+        return 0;
+    }
+    
+    /**
+     * Adds a given amount of components to a specific stoppoint inventory record, 
+     * it assumed you use findStoppointComponentDataID before calling this function.
+     * Does not persist any data
+     * @param stoppointInvDataID int value with the DataID pointing to the specific record being updated     
+     * @param componentAmount int value with the amount of components being added
+     * @param ses Open Session object
+     * @return A SolutionRecord with the updated amount
+     * @throws ValueException on invalid DataID
+     * @throws Exception on system error
+     */
+    private SolutionRecord addStoppointInvComponent(int stoppointInvDataID, int componentAmount, Session ses) throws Exception{
+        
+        int stoppointInvSolutionID = ses.getSolutionID(stoppointInvEntity);
+        
+        SolutionRecord sr = ses.getSolutionRecord(stoppointInvSolutionID, stoppointInvDataID);
+        if(sr.getInstanceID() == 0){
+            throw new ValueException("Invalid DataIDs");
+        }
+        
+        int newAmount = sr.getValueInteger(stoppointInvAmount) + componentAmount;
+        sr.setValueInteger(stoppointInvAmount, newAmount);
+        
+        return sr;
+    }
+    
+    
+    /**
+     * Removes a given amount of components to a specific stoppoint inventory record, if the amount is larger than installed nothing is removed,
+     * it assumed you use findStoppointComponentDataID before calling this function.
+     * Does not persist any data
+     * @param stoppointInvDataID int value with the DataID pointing to the specific record being updated     
+     * @param componentAmount int value with the amount of components being removed
+     * @param ses Open Session object
+     * @return A SolutionRecord with the updated amount
+     * @throws ValueException on invalid DataID
+     * @throws Exception on system error
+     */
+    private SolutionRecord removeStoppointInvComponent(int stoppointInvDataID, int componentAmount, Session ses) throws Exception{
+        
+        int stoppointInvSolutionID = ses.getSolutionID(stoppointInvEntity);
+        
+        SolutionRecord sr = ses.getSolutionRecord(stoppointInvSolutionID, stoppointInvDataID);
+        if(sr.getInstanceID() == 0){
+            throw new ValueException("Invalid DataIDs");
+        }
+        
+        int newAmount = sr.getValueInteger(stoppointInvAmount) - componentAmount;
+        if(newAmount >= 0){
+            sr.setValueInteger(stoppointInvAmount, newAmount);
+        }
+        return sr;
+    }
+    
+    /**
+     * Creates a new record in the stoppoint inventory entity with the given component and amount and a stoppoint.
+     * it assumed you use findStoppointComponentDataID before calling this function to ensure you do not create duplicates
+     * Does not persist data
+     * @param stoppointDataID int value with the stoppoint DataID
+     * @param componentDataIDint int value with the component DataID
+     * @param componentAmount int value with the amount being added
+     * @param ses Open Session Object
+     * @return SolutionRecordNew Object with the fields set
+     * @throws ValueException On invalid dataIDs
+     * @throws Exception on system error
+     */
+    private SolutionRecordNew createStoppointInvComponentRecord(int stoppointDataID, int componentDataID, int componentAmount, Session ses) throws Exception{
+        
+        int stoppointSolutionID = ses.getSolutionID(stoppointEntity);
+        int componentSolutionID = ses.getSolutionID(componentEntity);
+        
+        
+        SolutionRecord spSR = ses.getSolutionRecord(stoppointSolutionID, stoppointDataID);
+        SolutionRecord compSR = ses.getSolutionRecord(componentSolutionID, componentDataID);
+        if(spSR.getInstanceID() == 0 || compSR.getInstanceID() == 0){
+            throw new ValueException("Invalid DataIDs");
+        }
+
+        SolutionRecordNew srn = ses.getSolutionRecordNew(stoppointInvEntity);
+        srn.setReference(stoppointInvStoppoint, spSR);
+        srn.setReference(stoppointInvComponent, compSR);
+        srn.setValueInteger(stoppointInvAmount, componentAmount);
+        return srn;
+    }
+    
     
     
     /**
@@ -112,9 +405,7 @@ public class Activities extends CodeunitFormevents{
         else{
             throw new ValueException("Invalid DataIDs");
         }
-
         return deviceSR;
-
     }
     
     
@@ -144,6 +435,10 @@ public class Activities extends CodeunitFormevents{
         return deviceSR;
         
     }
+    
+    
+    
+    
     
     
     
---
+++ CallAPI.java
@@ -0,0 +1,250 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package moveitems;
+
+import dk.p2e.blanket.codeunit.CodeunitPagecontent;
+import dk.p2e.blanket.form.Command;
+import dk.p2e.blanket.form.Security;
+import dk.p2e.blanket.form.handler.QueryPart;
+import dk.p2e.util.Systemout;
+import dk.tempusserva.api.Session;
+import dk.tempusserva.api.SessionFactory;
+import dk.tempusserva.api.SolutionQuery;
+import dk.tempusserva.api.SolutionQueryResultSet;
+import dk.tempusserva.api.SolutionRecordNew;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Objects;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+/**
+ * Class for calling and parsing APIs from Axentia and in the future MultiQ
+ * Currently it is a CodeunitPagecontent for testing purposes, in the future change to a
+ * codeunit that runs on a set timer on Tempus Serva platform. 
+ * It is required that you use the compile flag -encoding UTF-8 to prevent encoding related errors
+ * @author XSMA
+ */
+public class CallAPI extends CodeunitPagecontent {
+    
+    private final int initStatusID = 95;
+
+
+    @Override
+    public String execute(Command cmnd, Security scrt, Hashtable hshtbl) {
+        String url = "https://ibusapi.axentia.se/api/Display/status/ok?api-version=1.0";
+        String apikey = "";
+        Session ses = SessionFactory.getSession(this);
+        try {
+            String res = callApi(url,apikey);
+            parseAxentia(res);
+        } catch (Exception ex) {
+            Systemout.println("Test");
+            Logger.getLogger(CallAPI.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        ses.close();
+        return "";
+
+    }
+
+    
+    
+    /**
+     * Performs a get call on a given url and api key. Meant to be used on axentia's api. Made generic enough for the future adding of MultiQ
+     * Axentias api is documented on https://ibusapi.axentia.se/swagger/v1/swagger.json 
+     * @param url String value with the url
+     * @param apikey String value with the apikey 
+     * @return String value with the response. 
+     * @throws Exception On failed 
+     */
+    private String callApi(String url, String apikey) throws Exception{
+        StringBuilder res = new StringBuilder();
+        
+        try{
+            CloseableHttpClient httpclient = HttpClients.createDefault();
+            HttpGet httpget = new HttpGet(url);
+            httpget.addHeader("apikey", apikey );
+            
+            HttpResponse httpresponse = httpclient.execute(httpget);
+
+            if(httpresponse.getStatusLine().getStatusCode() == 200){
+                Scanner sc = new Scanner(httpresponse.getEntity().getContent(),"UTF-8");
+                while(sc.hasNext()) {
+                    res.append(sc.nextLine());
+                }
+            }
+            else{
+                httpclient.close();
+                throw new Exception("callApi did not get statuscode 200");
+            }
+            httpclient.close();
+
+        }
+        catch(Exception e){
+            Systemout.println(e.getMessage());
+            Systemout.println("CallAPI function needs logging");
+        }
+        return res.toString();
+    }
+    
+    
+    /**
+     * Parses the axentia json that is returned from the API, the documentation for the 
+     * fields can be found on https://ibusapi.axentia.se/swagger/v1/swagger.json 
+     * @param json String with the json containing information from all modules
+     */
+    private void parseAxentia(String json){
+                            
+        try{
+            JSONArray jsonList = (JSONArray) new JSONParser().parse(json);
+            for(int i = 0; i < jsonList.size(); i++){
+                JSONObject jsonObj = (JSONObject) jsonList.get(i);
+                createAxentiaRecords(jsonObj);
+            }
+        }
+        catch(ParseException e){
+            Systemout.println("In parseAxentia");
+            Systemout.println(e.toString());
+        }
+    }
+    
+    /**
+     * Creates the TempusServa records from the json api string. Wrires to the 
+     * @param jsonObj JSONObject containing the information from 1 module
+     */
+    private void createAxentiaRecords(JSONObject jsonObj){
+        HashMap<String,String> stringValues = new HashMap<>();
+        long id = (long) jsonObj.get("ibusId");
+        long batteryStatus = (long) jsonObj.get("batteryStatus");
+        long uptime = (long) jsonObj.get("upTime");
+        long errorCode = (long) jsonObj.get("errorCode");
+        String displayMode = convertDisplayModeCode((long) jsonObj.get("displayMode"));
+        
+        //String values is simply to handle empty or null return values in the string fields.
+        stringValues.put("DEVICECOMMENTS", (String) jsonObj.get("displayComments"));
+        stringValues.put("FIRMWAREVERSION", (String) jsonObj.get("firmware"));
+        stringValues.put("BATTERYVOLTAGE", (String) jsonObj.get("batteryVoltage"));
+        stringValues.put("STOPPOINTID", (String) jsonObj.get("stopPointId"));
+        stringValues.put("STOPPOINTNAME", (String) jsonObj.get("stopPointName"));
+        stringValues.put("DESCRIPTION", (String) jsonObj.get("statusDescription"));
+        
+        String lastDataPackage = parseAxentiaDate((String) jsonObj.get("lastDataPacket"));
+        String timestamp = parseAxentiaDate((String) jsonObj.get("timeStamp"));
+
+        JSONObject position = (JSONObject) jsonObj.get("displayPosition");
+        Boolean isSet = (Boolean) position.get("isSet");
+        Double latitude = (Double) position.get("latitude");
+        Double longitude = (Double) position.get("longitude");
+        
+
+        
+        stringValues.values().removeIf(Objects::isNull);
+
+        Session session = SessionFactory.getSession(this);
+        try{
+                SolutionQuery query = session.getSolutionQuery("devices");
+                query.addWhereCriterion("SERIALNO",QueryPart.EQUALS, String.valueOf(id));
+                SolutionQueryResultSet executeQuery = query.executeQuery();
+                if(executeQuery.size() > 0){
+                    
+                    SolutionRecordNew record = session.getSolutionRecordNew("statuslog");
+                    stringValues.entrySet().forEach((entry) -> {
+                        String key = entry.getKey();
+                        String value = entry.getValue();
+                        record.setValue(key, value);
+                    });
+                    if(isSet){
+                        String pos = String.valueOf(latitude) + " " + String.valueOf(longitude);
+                        record.setValue("DEVICEPOSITION", pos);
+                    }
+                    
+                    record.setValue("TIMESTAMP", timestamp);
+                    record.setValue("LASTUPDATE", lastDataPackage);
+                    record.setValueInteger("BATTERYSTATUS", Math.toIntExact(batteryStatus));
+                    record.setValue("ERRORCODE", String.valueOf(errorCode));
+                    record.setValueInteger("UPTIME", Math.toIntExact(uptime));
+                    record.setReference("DEVICEID", executeQuery.getRecord(0));
+                    record.setValue("DISPLAYMODE", displayMode);
+                    record.setValueInteger("StatusID", initStatusID);
+                    record.persistChanges();     
+
+            }
+        }
+        catch (Exception e){
+            Systemout.println("createAxentiaRecords in CallAPI class: Temp error handling fix this");
+            Systemout.println(e.toString());
+        }
+        session.close();
+    }
+    
+
+    /**
+     * Parses an axentia date format into a Date object
+     * @param dateString String date to be converted to a date object
+     * @return A Date object with the value according to the string value, null if the string could not be parsed
+     */
+    private String parseAxentiaDate(String dateString){
+        List<SimpleDateFormat> dateFormats = new ArrayList<>();
+        
+        dateFormats.add(new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS"));
+        dateFormats.add(new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss"));
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+        for(SimpleDateFormat pattern : dateFormats){
+            try {
+                Date dt = pattern.parse(dateString);
+                String res = dateFormat.format(dt);
+                return res;
+            } 
+            catch (java.text.ParseException ex) {
+                //Do nothing next loop
+            }
+        }
+        
+        
+        
+        return "";
+    }
+    
+    
+    /**
+     * Convert a status code from axentia to its meaning
+     * @param statusCode Int value representing the status code
+     * @return A String that represents the corrosponding statuscode, if unknown statuscode empty string is returned
+     */
+    private String convertDisplayModeCode(long statusCode){
+        int code = Math.toIntExact(statusCode);
+        HashMap<Integer,String> convertionTable = new HashMap<>();
+        convertionTable.put(0, "Not mounted [disabled / off]");
+        convertionTable.put(1, "In Operation");
+        convertionTable.put(2,"In Operation [not mounted on site]");
+        convertionTable.put(3,"In Operation [demo / mobile]");
+        convertionTable.put(4,"Service / Repair");
+        convertionTable.put(5,"Mounted [disabled / off]");
+        
+        if(convertionTable.containsKey(code)){
+            return convertionTable.get(code);
+        }
+        return "";
+    }
+    
+}
+
+                    
+                    
